4f8cc244bcd3e94d886abfed41e46fb872e6a651
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2023-12-29 13:36:51 -0300

    add puppeteer

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import json2html from "node-json2html";
 import http from "http";
-import fs from "fs";
 import data from "./data.json" assert { type: "json" };
+import puppeteer from "puppeteer";
 const template = {
   "<>": "div",
   html: [
     { "<>": "h1", html: "${name}", style: "color: blue" },
     { "<>": "p", html: "${text}" },
+    { "<>": "img", src: "${img}", style: "width: 500px" },
   ],
   style:
     "display:flex; flex-direction:column; align-items:center; justify-content:center; height:100vh; background-color: #f5f5f5;",
@@ -19,5 +20,16 @@ const handleRequest = (request, response) => {
   response.write(json2html.render(data, template));
   response.end();
 };
+
+
+async function generatePDFfromHTML(htmlContent, outputPath) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(htmlContent);
+    await page.pdf({ path: outputPath, format: 'A4' });
+    await browser.close();
+  }
+
+generatePDFfromHTML(json2html.render(data, template), './test.pdf');
 
 http.createServer(handleRequest).listen(8000);
