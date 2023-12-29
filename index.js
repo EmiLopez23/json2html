496249877bcc6b06f02ec1e7412ed819583eb88c
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2023-12-29 13:03:26 -0300

    add styles

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ import data from "./data.json" assert { type: "json" };
 const template = {
   "<>": "div",
   html: [
-    { "<>": "h1", html: "${name}" },
+    { "<>": "h1", html: "${name}", style: "color: blue" },
     { "<>": "p", html: "${text}" },
   ],
+  style:
+    "display:flex; flex-direction:column; align-items:center; justify-content:center; height:100vh; background-color: #f5f5f5;",
 };
 
 const handleRequest = (request, response) => {
